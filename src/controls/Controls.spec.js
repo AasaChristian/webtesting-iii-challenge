a20ee1d0275bb422dd416b99b09a02f45f14401b
Aasa-Christian <aasachristian@yahoo.com>
2019-11-11 17:06:09 -0500

    created controls test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,10 @@
-// Test away!
+import React from 'react';
+import {render} from '@testing-library/react'
+
+import Controls from '../controls/Controls'
+
+
+test('Controls renders correctly', () => {
+    expect(render(<Controls/>)).toMatchSnapshot();
+    
+});
